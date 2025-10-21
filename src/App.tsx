4aa7359de176f1ac8086f21964b9c7499394b52e
Mas-Pey <konamexxxpes@gmail.com>
2025-10-21 23:33:19 +0700

    feat: implement event management with form inputs and monthly calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,79 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+import '@zach.codes/react-calendar/dist/calendar-tailwind.css';
+import {
+  MonthlyCalendar,
+  MonthlyNav,
+  MonthlyBody,
+  MonthlyDay,
+  DefaultMonthlyEventItem,
+} from '@zach.codes/react-calendar';
 
-function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-zinc-950">
-        Hello Pey! Tailwind is ready!
-      </h1>
-    </div>
-  )
+interface EventType {
+  name: string;
+  date: Date;
 }
 
-export default App
+export const MyMonthlyNav = () => {
+  // form inputs state
+  const [name, setName] = useState('');
+  const [date, setDate] = useState('');
+
+  const [currentMonth, setCurrentMonth] = useState<Date>(new Date());
+  const [events, setEvents] = useState<EventType[]>([
+    { name: 'John', date: new Date() },
+    { name: 'Jane', date: new Date() },
+  ]);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!name || !date) return;
+
+    const newEvent: EventType = {
+      name, 
+      date: new Date(date)
+    }
+
+    setEvents((prev => [...prev, newEvent]))
+  }
+
+  return (
+    <div style={{ margin: '0 auto', maxWidth: '800px', padding: '1rem' }}>
+      {/* Form Input */}
+      <h2>Add Event</h2>
+      <form onSubmit={handleSubmit} className='max-w-sm mx auto'>
+        <div className='mb-2'>
+          <label className='block mb-2 text-sm font-medium text-gray-900'>Name of Employee:</label>
+          <input type='text' value={name} onChange={(e) => setName(e.target.value)}/>
+        </div>
+        <div className='mb-2'>
+          <label>Date:</label>
+          <input type='date' value={date} onChange={(e) => setDate(e.target.value)}/>
+        </div>
+        <button type="submit" > Add Data </button>
+      </form>
+
+      {/* Monthly Calendar */}
+      <MonthlyCalendar
+        currentMonth={currentMonth}
+        onCurrentMonthChange={setCurrentMonth}
+      >
+        <MonthlyNav /> {/* default nav */}
+        <MonthlyBody events={events}>
+          <MonthlyDay<EventType>
+            renderDay={(dayEvents) =>
+              dayEvents.map((item, idx) => (
+                <DefaultMonthlyEventItem
+                  key={idx}
+                  title={item.name}
+                  date={format(item.date, 'k:mm')}
+                />
+              ))
+            }
+          />
+        </MonthlyBody>
+      </MonthlyCalendar>
+    </div>
+
+  );
+}
