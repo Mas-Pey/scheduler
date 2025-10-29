c36755591e227c7d1f0bd04bab66f1dfc88e09fa
Mas-Pey <konamexxxpes@gmail.com>
2025-10-29 23:46:43 +0700

    refactor: integrate EmployeeList with updateEmployee and updatingId

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,22 @@ import { ScheduleCalendar } from './components/ScheduleCalendar';
 
 function App() {
   const [currentMonth, setCurrentMonth] = useState<Date>(new Date())
-  const { employees, showList, fetchEmployees, setShowList } = useEmployees()
+  const {
+    employees,
+    showList,
+    updatingId,
+    fetchEmployees,
+    setShowList,
+    updateEmployee
+  } = useEmployees()
   const { schedules, createSchedule } = useSchedule()
 
   return (
     <div className='mx-auto max-w-5xl py-4'>
-      <button onClick={fetchEmployees} className='p-2 bg-amber-600 text-white text-md rounded-md hover:bg-amber-700'>
+      <button
+        onClick={fetchEmployees}
+        className='p-2 bg-amber-600 text-white text-md rounded-md hover:bg-amber-700'
+      >
         Load Employees
       </button>
 
@@ -20,6 +30,8 @@ function App() {
         employees={employees}
         show={showList}
         onHide={() => setShowList(false)}
+        updatingId={updatingId}
+        onUpdate={updateEmployee}
       />
 
       <ScheduleCalendar
